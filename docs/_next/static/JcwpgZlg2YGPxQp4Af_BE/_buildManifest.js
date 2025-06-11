175af4b41971e567b7a01de42809b0401c8a7f8d
Kathan <user@example.com>
2025-06-11 13:52:57 -0700

    Implement light/dark mode toggle in navigation header

---
+++ docs/_next/static/JcwpgZlg2YGPxQp4Af_BE/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/117-5447bb9b03654628.js","static/chunks/pages/index-88e149066235dc3a.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
