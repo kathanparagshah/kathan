6a42ed29b2b0d271f487bb7de2cd3124f6a71f75
Kathan <user@example.com>
2025-06-12 13:01:45 -0700

    Fix GitHub Pages configuration by removing basePath and assetPrefix

---
+++ docs/_next/static/rzg1vsy5won5k66wpRN-h/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/997-6e240d44068398fd.js","static/chunks/pages/index-40108e254e49c652.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
   output: 'export',
   distDir: 'docs',
-  basePath: isProd ? '/kathan' : '',
-  assetPrefix: isProd ? '/kathan/' : '',
+  basePath: '',
+  assetPrefix: '',
   trailingSlash: true,
   images: { unoptimized: true },
 };
